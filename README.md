59d40cc7d2b9945a7198f61f5d6c5fc95182f686
Matt Boutet <matt@bigroomstudios.com>
2017-11-29 20:33:06 -0500

    Initial commit, basic video face detection working

---
+++ README.md
@@ -0,0 +1,19 @@
+# OpenCV based video object detection & classification.
+
+OpenCV application written in Node.js.
+
+
+
+```bash
+$ git clone git@github.com:mattboutet/strangeBru.git strangeBru
+$ cd strangeBru
+$ brew tap homebrew/science
+$ brew install opencv@2
+$ brew link --force opencv@2
+$ npm install
+$ npm run video
+```
+
+To highlight faces detected in your webcam.
+
+Based on https://github.com/andrewda/node-opencv-examples
---
+++ index.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const CV = require('opencv');
+
+// Define constants
+const COLOR = [0, 255, 0];
+const THICKNESS = 2;
+
+// Create new camera and window instances
+const camera = new CV.VideoCapture(0);
+const window = new CV.NamedWindow('Video', 0);
+
+// Set camera resolution (250x250)
+camera.setWidth(250);
+camera.setHeight(250);
+
+// Update window every 100ms
+setInterval(() => {
+
+    // Read image from camera
+    camera.read((err, im) => {
+
+        if (err) {
+            throw err;
+        }
+
+        // Verify that image is present (larger than 0px height and width)
+        if (im.size()[0] > 0 && im.size()[1] > 0) {
+            // Detect faces using the built-in HAAR classifier
+            im.detectObject(CV.FACE_CASCADE, {}, (err, faces) => {
+
+                if (err) {
+                    throw err;
+                }
+
+                // Cycle through the returned faces
+                faces.forEach((face) => {
+                    // Draw a rectange around the face
+                    im.rectangle([face.x, face.y], [face.width, face.height], COLOR, THICKNESS);
+                });
+
+                // Show image in window
+                window.show(im);
+                window.blockingWaitKey(0, 50);
+            });
+        }
+    });
+}, 100);
